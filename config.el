d9558e459721569bc591c434c9da52da141ad988
mattmc3 <mattmc3@gmail.com>
2021-06-23 23:21:58 -0400

    Add helpful comments

--- config.el
+++ config.el
@@ -3,6 +3,9 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+;; Some helpful starting docs
+;; https://github.com/hlissner/doom-emacs/blob/develop/docs/getting_started.org#customize
+;; key mappings: https://github.com/hlissner/doom-emacs/blob/develop/docs/api.org#map
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
